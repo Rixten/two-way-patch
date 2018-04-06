846a5245abb986eb2a55fe48d77c96ae0df96e7c
alinaenni <alinaenni@gmail.com>
2018-04-06 22:27:43 +0100

    Trying to get the php code to work

--- counter.php
+++ counter.php
@@ -1,34 +1,24 @@
-<!-- this is a php file that I'm testing something with -->
+<!-- this is a php file -->
 
  <?php
 
-$list1 = array("angry", "awed", "curious", "excited", "sceptical", "scared");
-// angry0, awed1, curious2, excited3, sceptical4, scared5
+ // Check if the form is submitted
+ if ( isset( $_GET['submit'] ) ) {
 
-$list2 = array(0, 0, 0, 0, 0, 0);
+ // Retrieve the form data by using the element's name attributes value as key
+ $emotion = $_GET['emotion'];
 
-$count1 = 0;
-$count2 = 0;
+ // Array: angry0, awed1, curious2, excited3, sceptical4, scared5
+$labels = array("angry", "awed", "curious", "excited", "sceptical", "scared");
+$date = date('m/d/Y');  /* get current date */
 
-$angry = 0;
-$awed = 0;
-$curious = 0;
-$excited = 0;
-$sceptical = 0;
-$scared = 0;
 
-$emotion = $_POST['emotion']; // define variable emotion & assign data to it
+$fp = fopen("data.txt", "a"); /* open data file, append */
+$savestring = $labels[$emotion].", ".$date."n"; /* append data file */
+fwrite($fp,$savestring);
+fclose($fp);
 
-for ( $count1 = 0; $count1 < 6; $count1++ ) { // run through array 1
-  if ( $emotion == $list[$count1] ) { // if emotion string matches array element
-
-      for ( $count2 = 0; $count2 < 6; $count2++ ) { // run through array 2
-
-          if ( $count1 == $count2 ) {
-            $list2[$count2] = $list2[$count2] + 1; // increment number in array 2
-          }
-      }
-  }
 }
+
 
   ?>
